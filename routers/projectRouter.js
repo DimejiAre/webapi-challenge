f490c9b829ba12257d1c31dc85734d41b59bb39d
DimejiAre <olardi2@gmail.com>
2019-11-01 10:12:58 +0100

    add middleware for checking project body and post endpoint

--- routers/projectRouter.js
+++ routers/projectRouter.js
@@ -19,6 +19,21 @@ router.get('/:id', [validateProjectID], (req,res) => {
     res.status(200).json(req.project)
 })
 
+router.post('/', [validateProjectBody],(req,res) => {
+    const project = req.body;
+    db.insert(project)
+        .then(response => {
+            res.status(201).json(response)
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: `There was an error while saving the post to the database ${err}`
+            })
+        })
+})
+
+// Middleware
+
 function validateProjectID (req,res,next){
     const id = req.params.id;
 
@@ -39,6 +54,22 @@ function validateProjectID (req,res,next){
                 error: `Project information could not be retrieved ${err}`
             })
         })
+}
+
+function validateProjectBody(req,res,next){
+    if(Object.keys(req.body).length > 0){
+        if(req.body.name && req.body.description){
+            next();
+        } else {
+            res.status(400).json({
+                message: "missing required name or description field"
+            })
+        }
+    } else {
+        res.status(400).json({
+            message: "missing project data"
+        })
+    }
 }
 
 module.exports = router
