7dd8ceb58b47042cc8ec2fa5c907c999a9d4912a
DimejiAre <olardi2@gmail.com>
2019-11-01 10:47:55 +0100

    add middleware for checking action body and create endpoints for inserting,updating and deleting actions

--- routers/actionRouter.js
+++ routers/actionRouter.js
@@ -19,6 +19,52 @@ router.get('/:id', [validateActionId], (req,res) => {
     res.status(200).json(req.action)
 })
 
+router.post('/', [validateActionBody], (req,res) => {
+    const action = req.body;
+
+    db.insert(action)
+        .then(action => {
+            res.status(201).json(action)
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: `There was an error while saving the action to the database ${err}`
+            })
+        })
+
+})
+
+router.put('/:id', [validateActionId,validateActionBody], (req,res) => {
+    const id = req.params.id;
+    const action = req.body;
+
+    db.update(id,action)
+        .then(action => {
+            res.status(200).json(action)
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: `There was an error while saving the action to the database ${err}`
+            })
+        })
+
+})
+
+router.delete('/:id', [validateActionId], (req,res) => {
+    const id = req.params.id;
+
+    db.remove(id)
+        .then(response => {
+            res.status(200).json({
+                message: `${response} action deleted`
+            })
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: `There was an error while deleting the action from the database ${err}`
+            })
+        })
+})
 
 // Middleware
 
@@ -42,6 +88,22 @@ function validateActionId (req,res,next){
                 error: `Action information could not be retrieved ${err}`
             })
         })
+}
+
+function validateActionBody(req,res,next){
+    if(Object.keys(req.body).length > 0){
+        if(req.body.project_id && req.body.description && req.body.notes){
+            next();
+        } else {
+            res.status(400).json({
+                message: "missing required name, description, or notes field"
+            })
+        }
+    } else {
+        res.status(400).json({
+            message: "missing action data"
+        })
+    }
 }
 
 
